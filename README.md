52dac4f1bfb5feb71f0e04f5ea355301a5d3ecdb
Andrew Brown <brown_a1@hotmail.com>
2026-06-07 12:31:44 +0100

    Add files via upload

--- README.md
+++
@@ -1,2 +0,0 @@
-# CodePenStepSixTaskBrownA
-Coffee web-page created in CodePen for Step 6 of the Introduction To Digital course.
---
+++ script.js
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="./style.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pen</title>
+  </head>
+  <body>
+   
+    <h1>In Search of the Perfect Cup of Coffee</h1>
+     <img src="https://plus.unsplash.com/premium_photo-1675435644687-562e8042b9db?q=80&w=1498&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" height="600">
+    
+    <p>
+    Not everyone likes coffee, but not all cups of coffee are equal. <br>
+    A slug of instant caffeine shares very little in common with the best that coffee has to offer. <br>
+      The good news is that the explosion in the number of artisanal coffee roasters and coffee shops new trading on UK high-streets means that most of us can easily experience and afford a coffee epiphany. <br> 
+    </p>
+    <p>
+    But once you know how coffee should taste, how do you go about elevating your daily cuppa? <br>
+    At home, you might obsess about how to get the best from your chosen method of brewing. But you will never get a decent cup unless you give serious thought to what happened to the coffee before you spooned it into your coffee maker.<br>
+      
+    </p>
+You want a special brew? Start thinking about:
+    
+    <ul>
+    How the beans were processed before they got to the roaster. <br>
+    How the beans have been roasted (lighter roasts allow more complex flavours to come through). <br>
+    How long it was since the beans were roasted (grinding the beans too early after roasting makes for a thinner less satifying flavour). <br>
+    Think about grinding the beans just before you brew your coffee. It really makes a difference.<br>
+    Experiment with what brewing method gives you the extract that suits you (pour-over, stove-top mokka, coffee machine). <br>
+    </ul>
+
+    <p>
+      Each element in the process has its own rabbit hole that you can get lost in. <br>
+       </p>
+      My advice would be to: 
+      <ol>
+      1.Find a local roaster that you trust; someone you can discuss the sort of coffee you enjoy with (everyone has their own palate). <br>
+      2. Get yourself a good hand grinder (they are a lot cheaper that electric ones). <br>
+      3. Keep a week's supply of beans in a container that limits exposure to air and direct sunlight (store the rest in ziplock bags in the freezer). <br>
+      4. Use a stove-top mokka-pot if you like a strong cup of coffee (not the same as over-roasted). <br>
+      5. Try oat milk rather than cow (it gives a smoother texture).
+      
+      </ol>
+      
+   
+
+    <script src="./script.js"></script>
+  </body>
+</html>
